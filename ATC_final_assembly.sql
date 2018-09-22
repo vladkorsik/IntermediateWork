4e1dece955aeb07b7ba15915c4c2aa87c8789e97
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2018-09-21 20:07:52 -0400

    Update ATC_final_assembly.sql

--- ATC_final_assembly.sql
+++ ATC_final_assembly.sql
@@ -1,18 +1,28 @@
 drop table final_assembly;
-create table final_assembly 
+create table final_assembly
 as
 select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '2' as order
+from atc_to_drug_1 a
+join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
+join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
+join concept c on c.concept_id = descendant_concept_id and vocabulary_id like 'RxNorm%' and c.standard_concept = 'S'
+  ;
+
+insert into final_assembly
+select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '3' as order
 from atc_to_drug_2 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
-join concept c on c.concept_id = descendant_concept_id and c.vocabulary_id = 'RxNorm' and c.concept_class_id not like '%Group%'-- temporary exclude RxE
+join concept c on c.concept_id = descendant_concept_id  and c.vocabulary_id like 'RxNorm%' and c.standard_concept = 'S'
+where descendant_concept_id not in (select concept_id from final_assembly)
 ;
+
 insert into final_assembly
 select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '3' as order
 from atc_to_drug_3 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
-join concept c on c.concept_id = descendant_concept_id  and c.vocabulary_id = 'RxNorm' and c.concept_class_id not like '%Group%' -- temporary exclude RxE
+join concept c on c.concept_id = descendant_concept_id  and c.vocabulary_id like 'RxNorm%' and c.standard_concept = 'S'
 where descendant_concept_id not in (select concept_id from final_assembly)
 ;
 insert into final_assembly
@@ -20,9 +30,17 @@ select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '4' as o
 from atc_to_drug_4 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
-join concept c on c.concept_id = descendant_concept_id  and c.vocabulary_id = 'RxNorm'  and c.concept_class_id not like '%Group%'-- temporary exclude RxE
+join concept c on c.concept_id = descendant_concept_id  and c.vocabulary_id like 'RxNorm%' and c.standard_concept = 'S'
 where descendant_concept_id not in (select concept_id from final_assembly)
 ;
+
+delete final_assembly where concept_name like '%insulin%';
+insert into final_assembly
+select distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '5' as order
+from manual m
+join atc_drugs_scraper s using (atc_code)
+join devv5.concept_ancestor ca on ca.ancestor_concept_id = m.concept_id
+join concept c on c.concept_id = ca.descendant_concept_id;
 
 --table wo ancestor
 drop table final_assembly_woCA;
@@ -52,3 +70,10 @@ from atc_to_drug_4 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 where atc_code not in 
 (select atc_code from final_assembly_woCA);
+
+delete final_assembly where concept_name like '%insulin%';
+insert into final_assembly
+select distinct s.*, m.concept_id, m.concept_name, m.concept_class_id, '5' as order
+from manual m
+join atc_drugs_scraper s using (atc_code)
+;
