87ab386a089ab9345d9f7e836b1b16c2822f2bb6
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2018-10-01 23:34:57 -0400

    Update ATC_INTM_FA.sql

--- ATC/ATC_INTM_FA.sql
+++ ATC/ATC_INTM_FA.sql
@@ -268,6 +268,10 @@ where concept_code_1 ~ 'N02BE71' and concept_name ~* 'Dipyrocetyl|Bucetin|Phenac
 delete from  atc_to_drug_3
 where concept_code_1 ~ 'N02' and concept_name ~ 'Codeine' and not atc_name ~ 'codeine';
 
+--atenolol and other diuretics, combinations, one of a kind
+delete from atc_to_drug_3
+where concept_code_1 ~ 'C07CB53' and regexp_count(concept_name,'/')<2;
+
 delete from atc_to_drug_3 where  concept_id in --removing duplicates 
 (select concept_id from atc_to_drug_1);
 
