d12abd797849291c32d909fb327fe332005e7ad4
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2018-10-08 13:25:12 -0400

    add another class of pack contraceptives

--- ATC/ATC_final_assembly.sql
+++ ATC/ATC_final_assembly.sql
@@ -1,14 +1,14 @@
 drop table final_assembly;
 create table final_assembly
 as
-select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '2' as order
+select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '1' as order
 from atc_to_drug_1 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
 join concept c on c.concept_id = descendant_concept_id and vocabulary_id like 'RxNorm%' and c.standard_concept = 'S'
  ;
 delete from final_assembly
-where atc_code like 'G03FB%' and concept_class_id not like '%Pack%';
+where atc_code ~ 'G03FB|G03AB' and concept_class_id not like '%Pack%';
 
 insert into final_assembly
 select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '2' as order
