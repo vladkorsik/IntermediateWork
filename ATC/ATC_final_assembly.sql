72885a51367a5a0c8a103950d2de01f1c57e7243
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2018-11-08 14:36:56 -0500

    Update ATC_final_assembly.sql

--- ATC/ATC_final_assembly.sql
+++ ATC/ATC_final_assembly.sql
@@ -1,7 +1,7 @@
 drop table final_assembly;
 create table final_assembly
 as
-select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '1' as order
+select  distinct s.*, c.concept_id, c.concept_name,  c.concept_code, c.concept_class_id, '1' as order
 from atc_to_drug_1 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
@@ -11,7 +11,7 @@ delete from final_assembly
 where atc_code ~ 'G03FB|G03AB' and concept_class_id not like '%Pack%';
 
 insert into final_assembly
-select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '2' as order
+select  distinct s.*, c.concept_id, c.concept_name,  c.concept_code, c.concept_class_id, '2' as order
 from atc_to_drug_2 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
@@ -20,7 +20,7 @@ where descendant_concept_id not in (select concept_id from final_assembly)
 ;
 
 insert into final_assembly
-select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '3' as order
+select  distinct s.*, c.concept_id, c.concept_name, c.concept_code, c.concept_class_id, '3' as order
 from atc_to_drug_3 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
@@ -28,7 +28,7 @@ join concept c on c.concept_id = descendant_concept_id  and c.vocabulary_id like
 where descendant_concept_id not in (select concept_id from final_assembly)
 ;
 insert into final_assembly
-select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '4' as order
+select  distinct s.*, c.concept_id, c.concept_name, c.concept_code, c.concept_class_id, '4' as order
 from atc_to_drug_4 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
@@ -37,7 +37,7 @@ where descendant_concept_id not in (select concept_id from final_assembly)
 ;
 
 insert into final_assembly
-select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '5' as order
+select  distinct s.*, c.concept_id, c.concept_name, c.concept_code, c.concept_class_id, '5' as order
 from atc_to_drug_5 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
@@ -46,7 +46,7 @@ where descendant_concept_id not in (select concept_id from final_assembly)
 ;
 
 insert into final_assembly
-select  distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '6' as order
+select  distinct s.*, c.concept_id, c.concept_name,  c.concept_code, c.concept_class_id, '6' as order
 from atc_to_drug_6 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 join devv5.concept_ancestor on ancestor_concept_id = a.concept_id
@@ -56,7 +56,7 @@ where descendant_concept_id not in (select concept_id from final_assembly)
 
 delete from final_assembly where atc_name like '%insulin%';
 insert into final_assembly
-select distinct s.*, c.concept_id, c.concept_name, c.concept_class_id, '7' as order
+select distinct s.*, c.concept_id, c.concept_name, c.concept_code, c.concept_class_id, '7' as order
 from atc_to_drug_manual m
 join atc_drugs_scraper s using (atc_code)
 join devv5.concept_ancestor ca on ca.ancestor_concept_id = m.concept_id
@@ -70,12 +70,12 @@ and concept_id in (select concept_id from final_assembly group by concept_id hav
 drop table final_assembly_woCA;
 create table final_assembly_woCA 
 as
-select  distinct atc_code, a.atc_name, a.concept_id, a.concept_name, a.concept_class_id, '1' as order
+select  distinct atc_code, a.atc_name, a.concept_id, a.concept_name,  a.concept_code, a.concept_class_id, '1' as order
 from atc_to_drug_1 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 ;
 insert into final_assembly_woCA
-select  distinct atc_code, a.atc_name, a.concept_id, a.concept_name, a.concept_class_id, '2' as order
+select  distinct atc_code, a.atc_name, a.concept_id, a.concept_name,  a.concept_code, a.concept_class_id, '2' as order
 from atc_to_drug_2 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 where atc_code not in 
@@ -84,28 +84,28 @@ and a.concept_id not in
 (select concept_id from final_assembly_woCA)
 ;
 insert into final_assembly_woCA
-select  distinct atc_code, a.atc_name, a.concept_id, a.concept_name, a.concept_class_id, '3' as order
+select  distinct atc_code, a.atc_name, a.concept_id, a.concept_name,  a.concept_code, a.concept_class_id, '3' as order
 from atc_to_drug_3 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 where atc_code not in 
 (select atc_code from final_assembly_woCA)
 ;
 insert into final_assembly_woCA
-select  distinct atc_code,  a.atc_name, a.concept_id, a.concept_name, a.concept_class_id, '4' as order
+select  distinct atc_code,  a.atc_name, a.concept_id, a.concept_name,  a.concept_code, a.concept_class_id, '4' as order
 from atc_to_drug_4 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 where atc_code not in 
 (select atc_code from final_assembly_woCA);
 
 insert into final_assembly_woCA
-select  distinct atc_code,  a.atc_name, a.concept_id, a.concept_name, a.concept_class_id, '5' as order
+select  distinct atc_code,  a.atc_name, a.concept_id, a.concept_name, a.concept_code, a.concept_class_id, '5' as order
 from atc_to_drug_5 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 where atc_code not in 
 (select atc_code from final_assembly_woCA);
 
 insert into final_assembly_woCA
-select  distinct atc_code,  a.atc_name, a.concept_id, a.concept_name, a.concept_class_id, '6' as order
+select  distinct atc_code,  a.atc_name, a.concept_id, a.concept_name, a.concept_code, a.concept_class_id, '6' as order
 from atc_to_drug_6 a
 join atc_drugs_scraper s on substring (concept_code_1,'\w+')=atc_code
 where atc_code not in 
@@ -113,13 +113,14 @@ where atc_code not in
 
 delete from final_assembly_woca where atc_name like '%insulin%';
 insert into final_assembly_woca
-select distinct atc_code,m.atc_name, m.concept_id, m.concept_name, m.concept_class_id, '7' as order
+select distinct atc_code,m.atc_name, m.concept_id, m.concept_name, c.concept_code, m.concept_class_id, '7' as order
 from atc_to_drug_manual m
 join atc_drugs_scraper s using (atc_code)
+join concept c using(concept_id)
 ;
 
 insert into final_assembly_woca
-select f.atc_code, f.atc_name, c.concept_id, c.concept_name, c.concept_class_id, f.order
+select f.atc_code, f.atc_name, c.concept_id, c.concept_name, c.concept_code, c.concept_class_id, f.order
 from final_assembly_woca f
 join devv5.concept_ancestor ca
 on ca.ancestor_concept_id = f.concept_id
