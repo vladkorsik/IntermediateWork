3686cbf24d30dfb8904b91fbff4b51b745a61d65
Alexander <32357692+Alexdavv@users.noreply.github.com>
2019-03-19 22:35:11 +0300

    Update 1.sql

--- EU Vocabularies/1.sql
+++ EU Vocabularies/1.sql
@@ -26,3 +26,22 @@ WHERE EXISTS
             AND cr.relationship_id = 'Maps to'
             AND cr.invalid_reason IS NULL
       );
+
+
+
+
+SELECT t.source_code, t.concept_name, c.concept_name
+FROM tested_vocabulary t
+
+JOIN concept c
+         ON c.concept_code = t.source_code
+             AND c.vocabulary_id in ('ICD10', 'ICD10CM')
+
+WHERE NOT exists(
+      SELECT 1
+      FROM concept cc
+      WHERE t.source_code = cc.concept_code
+      AND cc.vocabulary_id in ('ICD10')
+    )
+ORDER BY RANDOM ()
+LIMIT 1000;
