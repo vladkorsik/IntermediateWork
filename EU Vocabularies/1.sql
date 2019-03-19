4ab37379ff26b1f04c253350b5ef4d1108555d2d
Alexander <32357692+Alexdavv@users.noreply.github.com>
2019-03-19 21:38:59 +0300

    Update 1.sql

--- EU Vocabularies/1.sql
+++ EU Vocabularies/1.sql
@@ -4,8 +4,25 @@ FROM (
     FROM tested_vocabulary
          ) as a
 WHERE EXISTS
-      (select 1
+      (SELECT 1
       FROM concept c
       WHERE a.concept_code = c.concept_code
             AND c.vocabulary_id in ('ICD10', 'ICD10CM')
-);
+      );
+
+
+SELECT COUNT (*)
+FROM (
+    SELECT DISTINCT source_code as concept_code
+    FROM tested_vocabulary
+         ) as a
+WHERE EXISTS
+      (SELECT 1
+      FROM concept c
+            JOIN concept_relationship cr
+                  ON c.concept_id = cr.concept_id_1
+      WHERE a.concept_code = c.concept_code
+            AND c.vocabulary_id in ('ICD10', 'ICD10CM')
+            AND cr.relationship_id = 'Maps to'
+            AND cr.invalid_reason IS NULL
+      );
