baca2c5b57f099631b6413e20672ab31e348fe3d
olegzhuk <oleg.zhuk@odysseusinc.com>
2019-10-02 18:48:42 +0300

    Everything except concept_relationship done

--- LOINC/attributes.sql
+++ LOINC/attributes.sql
@@ -6,9 +6,10 @@ LoincPartLink - contains all links between LOINC attributes and LOINC concepts
 Both files can be found on https://loinc.org/downloads/accessory-files/
 */
 
---TODO: Should we edit names of Loinc parts to remove unnecessary dots, etc?
-
+--Step 0
 --Run query to recreate devv5 in your schema
+
+--STEP 1: Additional sources uploading
 
 --DROP TABLE Loinc_PartLink;
 CREATE TABLE Loinc_PartLink (
@@ -42,29 +43,61 @@ CREATE TABLE LOINC_Part (
 SELECT * FROM LOINC_Part;
 
 
---STEP 2: Tables populating
+--STEP 2: Inserting into concept and concept_relationship
+--Populating concept_stage
+with s AS (SELECT DISTINCT pl.PartNumber, pl.PartName
+FROM Loinc_PartLink pl
+JOIN LOINC_Part p
+ON pl.PartNumber = p.PartNumber
+WHERE pl.LinkTypeName IN ('Primary', 'DetailedModel', 'SyntaxEnhancement')
+  AND pl.PartTypeName IN ('SYSTEM', 'METHOD', 'PROPERTY', 'TIME', 'COMPONENT', 'SCALE')
+AND pl.PartNumber NOT IN (SELECT DISTINCT concept_code FROM concept_stage)
+AND p.Status = 'ACTIVE')
 
---Populating with new attributes that are not already present in CDM, which are everything except Components
 --INSERT INTO concept_stage
-SELECT DISTINCT 0 AS concept_id,
-                s.partdisplayname AS concept_name,
+SELECT DISTINCT s.PartName AS concept_name,
                 'Measurement' AS domain_id,
                 'LOINC' AS vocabulary_id,
                 'LOINC Attribute' AS concept_class_id,
                 'C' AS standard_concept,                --Classification
-                s.partnumber AS concept_code,
+                s.PartNumber AS concept_code,
                 '1970-01-01'::date as valid_start_date, --Valid start date should be the date of last update
                 '2099-12-31'::date as valid_end_date,
                 NULL AS invalid_reason
-FROM LOINC_Part s
-WHERE s.parttypename != 'COMPONENT'
-AND s.partdisplayname NOT IN ('-', '*')
-AND s.status = 'ACTIVE';
+FROM s
+;
 
---TODO: Create new concept relationships (see file on google sheets)
---Populating concept_relationships
+--Populating concept_synonym
+with s AS (SELECT DISTINCT pl.PartNumber, p.PartDisplayName
+FROM Loinc_PartLink pl
+JOIN LOINC_Part p
+ON pl.PartNumber = p.PartNumber
+WHERE pl.LinkTypeName IN ('Primary', 'DetailedModel', 'SyntaxEnhancement')
+  AND pl.PartTypeName IN ('SYSTEM', 'METHOD', 'PROPERTY', 'TIME', 'COMPONENT', 'SCALE')
+AND pl.PartNumber NOT IN (SELECT DISTINCT concept_code FROM concept_stage)
+AND p.Status = 'ACTIVE'
+AND pl.PartName != p.PartDisplayName)
 
-WITH s AS (SELECT DISTINCT loincnumber, LongCommonName, PartNumber, PartName, PartTypeName FROM Loinc_PartLink)
+--INSERT INTO concept_synonym_stage
+SELECT DISTINCT s.PartDisplayName AS synonym_name,
+    s.PartNumber AS synonym_concept_code,
+    'LOINC' AS synonym_vocabulary_id,
+    4180186 AS language_concept_id --English language
+FROM s
+;
+
+SELECT DISTINCT LoincNumber, PartNumber, PartTypeName
+FROM Loinc_PartLink
+WHERE LinkTypeName IN ('Primary', 'DetailedModel', 'SyntaxEnhancement')
+AND PartTypeName = 'COMPONENT'
+GROUP BY LoincNumber, PartNumber, PartTypeName, LinkTypeName
+ORDER BY LoincNumber
+;
+
+
+
+
+WITH s AS (SELECT DISTINCT loincnumber, PartNumber, PartTypeName FROM Loinc_PartLink)
 
 --INSERT INTO concept_relationship_stage
 SELECT DISTINCT c.concept_id AS concept_id_1,
@@ -113,28 +146,9 @@ SELECT DISTINCT partname FROM Loinc_PartLink WHERE PartTypeName = 'SCALE';
 SELECT * FROM Loinc_PartLink WHERE partname = '{Setting}'
 ORDER BY LoincNumber;
 
---These LOINC codes potentially have 'double' concept relationships
-/*
-10000-8
-10000-8
-10001-6
-10001-6
-10002-4
-10002-4
-10003-2
-10003-2
-10004-0
-10004-0
-*/
 
 
---Currently we create concept for each record with different concept_code.
---TODO: Maybe use different approach? Any other approach will lead to problem with concept_codes but also removes excessive concepts from CDM
-SELECT *
-FROM LOINC_Part p
-WHERE p.partname IN
-      (SELECT p.partname FROM LOINC_Part GROUP BY p.partname HAVING COUNT(*) > 1)
-ORDER BY p.partname;
+
 
 --ANALYSIS OF COMPONENTS THAT ARE NOT CURRENTLY PRESENT IN CDM
 --Components not present in devv5 but present in Loinc_Parts
@@ -161,6 +175,7 @@ SELECT *
 FROM LOINC_Part s
 LEFT JOIN sources.loinc_hierarchy lh
 ON s.partnumber = lh.immediate_parent
+   OR s.partnumber = lh.code
 WHERE s.partnumber ~~* 'LP%'
 AND s.parttypename = 'COMPONENT'
 AND s.status = 'ACTIVE'
