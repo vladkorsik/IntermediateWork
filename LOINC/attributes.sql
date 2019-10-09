774c9d3ba5a9a9afbc6ad80007da4adcbed8d439
olegzhuk <oleg.zhuk@odysseusinc.com>
2019-10-09 18:14:25 +0300

    LOINC code combined with load_stage

--- LOINC/attributes.sql
+++ LOINC/attributes.sql
@@ -37,20 +37,27 @@ CREATE TABLE LOINC_Part (
 --STEP 2: Inserting into concept, concept_synonym and concept_relationship
 
 --Populating concept_stage
-with s AS (SELECT DISTINCT pl.PartNumber, p.PartDisplayName
+with s AS (SELECT DISTINCT pl.PartNumber, p.PartDisplayName, pl.parttypename
 FROM sources.loinc_partlink pl
 JOIN sources.loinc_part p
 ON pl.PartNumber = p.PartNumber
 WHERE pl.LinkTypeName IN ('Primary')    --or any other too
   AND pl.PartTypeName IN ('SYSTEM', 'METHOD', 'PROPERTY', 'TIME', 'COMPONENT', 'SCALE')
 AND pl.PartNumber NOT IN (SELECT DISTINCT concept_code FROM concept_stage WHERE concept_code IS NOT NULL)     --to exclude duplicates inserted during previous load_stage code
-AND p.Status = 'ACTIVE')
+)
 
---INSERT INTO concept_stage
-SELECT DISTINCT s.PartDisplayName AS concept_name,
+INSERT INTO concept_stage (concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+SELECT DISTINCT trim(s.PartDisplayName) AS concept_name,
                 'Measurement' AS domain_id,
                 'LOINC' AS vocabulary_id,
-                'LOINC Attribute' AS concept_class_id,
+                CASE WHEN s.parttypename = 'SYSTEM' THEN 'LOINC System'
+                     WHEN s.parttypename = 'METHOD' THEN 'LOINC Method'
+                     WHEN s.parttypename = 'PROPERTY' THEN 'LOINC Property'
+                     WHEN s.parttypename = 'TIME' THEN 'LOINC Time'
+                     WHEN s.parttypename = 'COMPONENT' THEN 'LOINC Component'
+                     WHEN s.parttypename = 'SCALE' THEN 'LOINC Scale'
+                     ELSE 'LOINC Attribute'             --To check, should be 0 rows with this concept_class_id
+                     END AS concept_class_id,
                 'C' AS standard_concept,                --Classification
                 s.PartNumber AS concept_code,
                 '1970-01-01'::date as valid_start_date, --Valid start date should be the date of last update
@@ -67,11 +74,11 @@ JOIN sources.loinc_part p
 ON pl.PartNumber = p.PartNumber
 WHERE pl.LinkTypeName IN ('Primary')
   AND pl.PartTypeName IN ('SYSTEM', 'METHOD', 'PROPERTY', 'TIME', 'COMPONENT', 'SCALE')
-AND pl.PartNumber NOT IN (SELECT DISTINCT concept_code FROM concept_stage WHERE concept_code IS NOT NULL)
-AND p.Status = 'ACTIVE'
-AND pl.PartName != p.PartDisplayName)
+AND pl.PartNumber IN (SELECT DISTINCT concept_code FROM concept_stage WHERE concept_code IS NOT NULL)
+AND pl.PartName != p.PartDisplayName
+and p.partnumber NOT IN (SELECT DISTINCT synonym_concept_code FROM concept_synonym_stage WHERE synonym_concept_code IS NOT NULL))
 
---INSERT INTO concept_synonym_stage
+INSERT INTO concept_synonym_stage (synonym_name, synonym_concept_code, synonym_vocabulary_id, language_concept_id)
 SELECT DISTINCT s.PartName AS synonym_name,
     s.PartNumber AS synonym_concept_code,
     'LOINC' AS synonym_vocabulary_id,
@@ -86,9 +93,11 @@ WITH s AS (SELECT DISTINCT loincnumber, p.PartNumber, p.PartTypeName
            ON pl.PartNumber = p.PartNumber
            WHERE pl.LinkTypeName IN ('Primary')
            AND pl.PartTypeName IN ('SYSTEM', 'METHOD', 'PROPERTY', 'TIME', 'COMPONENT', 'SCALE')
-           AND p.Status = 'ACTIVE')
+           AND pl.PartNumber IN (SELECT DISTINCT concept_code FROM concept_stage)
+           AND loincnumber IN (SELECT DISTINCT concept_code FROM concept_stage)
+           )
 
---INSERT INTO concept_relationship_stage
+INSERT INTO concept_relationship_stage (concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id, valid_start_date, valid_end_date, invalid_reason)
 SELECT DISTINCT s.loincnumber AS concept_code_1,
                 partnumber AS concept_code_2,
                 'LOINC' AS vocabulary_id_1,
